ef3b461159991f16007b98e4f1cd789450427481
SwapnilSkr <swapnilmkab@gmail.com>
2025-12-11 22:49:16 +0530

    added strictly add types

---
+++ objects/strictly_type_an_object.ts
@@ -0,0 +1,43 @@
+// Declare a Caterer type
+type Caterer = {
+  name: string;
+  address: string;
+  phone: number;
+};
+
+// Declare a Seats type
+type Seats = {
+  [keyof: string]: string;
+};
+
+// Declare a Airplane Type
+type Airplane = {
+  model: string;
+  flightNumber: string;
+  timeOfDeparture: Date;
+  timeOfArrival: Date;
+  caterer: Caterer;
+  seats: Seats;
+};
+
+// Assign Airplane Type to Object
+let airplane: Airplane = {
+  model: "Airbus A380",
+  flightNumber: "A2201",
+  timeOfDeparture: new Date(),
+  timeOfArrival: new Date(),
+  caterer: {
+    name: "Special Food Ltd",
+    address: "484, Some Street, New York",
+    phone: 7867856751,
+  },
+  seats: {
+    A1: "John Doe",
+    A2: "Mark Doe",
+    A3: "Sam Doe",
+  },
+};
+
+export function logAirplaneDetails() {
+  console.log("Airplane Details:", airplane);
+}

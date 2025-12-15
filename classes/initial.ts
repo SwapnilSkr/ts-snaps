0b43ac19b7fd7f5a5585e01ad6f7f75a806ce480
SwapnilSkr <swapnilmkab@gmail.com>
2025-12-15 17:37:48 +0530

    added classes and practices for classes as well

---
+++ classes/initial.ts
@@ -0,0 +1,65 @@
+//using boilerplate code for normal class definition and initialization
+class User {
+  //Not needed, use the shorthand instead
+  //   protected id: number;
+  //   public firstName: string;
+  //   public lastName?: string;
+  //   public readonly email: string;
+  //   protected salary: number;
+
+  //   constructor(
+  //     id: number,
+  //     firstName: string,
+  //     email: string,
+  //     salary: number,
+  //     lastName: string
+  //   ) {
+  //     this.id = id;
+  //     this.firstName = firstName;
+  //     this.lastName = lastName;
+  //     this.email = email;
+  //     this.salary = salary;
+  //   }
+
+  constructor(
+    protected id: number,
+    public firstName: string,
+    public readonly email: string,
+    protected salary: number,
+    public lastName: string
+  ) {}
+
+  public getFullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  //getters
+  public get getSalaryDetails(): number {
+    return this.salary;
+  }
+
+  //setters
+  public set setSalary(salary: number) {
+    if (salary > 0) this.salary = salary;
+    else throw new Error("This is not a valid salary you fucking doughnut");
+  }
+}
+
+class Admin extends User {}
+
+const user1 = new User(1, "Swapnil", "swapnilmkab@gmail.com", 70000, "Sarkar");
+user1.setSalary = 120000;
+
+//Not possible
+//user1.email = "some value"
+
+const fullName = user1.getFullName();
+const salaryOfUser = user1.getSalaryDetails;
+
+export function logUserDetails() {
+  console.log("user 1: ", user1);
+  console.log("Full Name: ", fullName);
+  console.log("Salary Details: ", salaryOfUser);
+  //Property 'id' is protected and only accessible within class 'User' and its subclasses.
+  //console.log(user1.id)
+}
--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import { userObjectLogs } from "./primitives/symbol.js";
 import { logPersons } from "./type-system/type_alias.js";
+import { employeeLogger } from "./practice/classes.js";
+import { logUserDetails } from "./classes/initial.js";
 
 userObjectLogs();
 logPersons();
+employeeLogger();
+logUserDetails();

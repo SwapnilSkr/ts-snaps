aa300f961e18a519fa24683116262f0f53423e42
SwapnilSkr <swapnilmkab@gmail.com>
2026-01-01 22:39:52 +0530

    added abstract classes

--- classes/abstract_classes.ts
+++ classes/abstract_classes.ts
@@ -5,13 +5,21 @@ type Holiday = {
 
 abstract class Department {
   protected abstract holidays: Holiday[];
-  constructor(protected name: string) {}
+  protected constructor(protected name: string) {}
 }
 
 class ITDepartment extends Department {
   protected holidays: Holiday[] = [];
+  constructor() {
+    super("IT Department");
+  }
 }
 
 class HRDepartment extends Department {
   protected holidays: Holiday[] = [];
+  constructor() {
+    super("HR Department");
+  }
 }
+
+const itDepartment: ITDepartment = new ITDepartment();
